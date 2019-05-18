810959aa0fd15d1f00e87508f56f53c253e8a562
Emilian Cole <emiliancole@gmail.com>
2019-05-18 23:53:59 +0200

    Update linePerpendicular.php

--- linePerpendicular.php
+++ linePerpendicular.php
@@ -1,4 +1,7 @@
 <?php 
+// given the line p1=(x1,y1), p2=(x2,y2)
+// and the point p3=(x3,y3), the formula calculates
+// the perpendicular intersection on the line from p3.
 
 $x1=50; $y1=50; $x2=100; $y2=100;
 $x3=70; $y3=70;
