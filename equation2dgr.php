b27cf72641dc12f8a701beba89a88225f4b3895b
Emilian Cole <emiliancole@gmail.com>
2019-05-18 22:59:53 +0200

    Add files via upload

---
+++ equation2dgr.php
@@ -0,0 +1,33 @@
+<?php 
+
+echo("2 degree Equation<hr>");
+   
+ $a=4;
+ $b=4;
+ $c=1;
+    
+   // y = a*x^2+b*x+c
+$d=pow($b,2)-4*$a*$c;
+$e=(-$b+sqrt($d))/2*$a;
+$f=(-$b+sqrt($d))/2*$a;
+   
+   
+ if($a<>0)
+    {
+        
+    if($d>0)
+    echo("Le soluzioni sono $e e $f");
+        
+        if($d==0)
+        
+echo("Le soluzioni è $e ");
+        
+        
+if($d<0)
+       
+ echo("La soluzione è impossibile ");
+    }
+    
+    
+
+ ?>
