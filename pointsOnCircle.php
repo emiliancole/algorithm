1f82de6570c6c0f8c967c761a9a839e6c21dee6f
Emilian Cole <emiliancole@gmail.com>
2019-05-19 00:00:25 +0200

    Add files via upload

---
+++ pointsOnCircle.php
@@ -0,0 +1,31 @@
+<?php
+
+function positionX($numItems,$thisNum){
+  $alpha = 360/$numItems; // angle between the elements
+  $r = 1000; // radius
+  $angle = $alpha * $thisNum; // angle for N element
+  $x = $r * cos(deg2rad($angle)); // X coordinates
+  return $x;
+}
+
+function positionY($numItems,$thisNum){
+  $alpha = 360/$numItems; // angle between the elements
+  $r = 1000; // radius
+  $angle = $alpha * $thisNum; // angle for N element
+  $y = $r * sin(deg2rad($angle)); // Y coordinates
+  return $y;
+}
+
+echo round(positionX(4,1))."<br>";
+echo round(positionY(4,1))."<br><br>";
+
+echo round(positionX(4,2))."<br>";
+echo round(positionY(4,2))."<br><br>";
+
+echo round(positionX(4,3))."<br>";
+echo round(positionY(4,3))."<br><br>";
+
+echo round(positionX(4,4))."<br>";
+echo round(positionY(4,4))."<br><br>";
+
+?>
